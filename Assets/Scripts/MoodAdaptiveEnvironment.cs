16fce0959fc988f4c24d87713a669578ca8d4220
Ken Marold <kenmarold@gmail.com>
2025-03-24 13:05:38 -0500

    Initial commit

---
+++ Assets/Scripts/MoodAdaptiveEnvironment.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class MoodAdaptiveEnvironment : MonoBehaviour
+{
+    public Light directionalLight;
+    public AudioSource ambientMusic;
+    public AudioClip calmClip;
+    public AudioClip intenseClip;
+    public Transform player;
+
+    public float speedThreshold = 5f;
+    public int enemiesDefeatedRecently = 0; // Hook this up to your enemy defeat logic
+
+    private Vector3 lastPosition;
+    private float currentSpeed;
+
+    void Start()
+    {
+        lastPosition = player.position;
+        ambientMusic.loop = true;
+        ambientMusic.playOnAwake = false;
+        ambientMusic.clip = calmClip;
+        ambientMusic.Play();
+    }
+
+    void Update()
+    {
+        CalculatePlayerSpeed();
+        UpdateEnvironment();
+    }
+
+    void CalculatePlayerSpeed()
+    {
+        currentSpeed = (player.position - lastPosition).magnitude / Time.deltaTime;
+        lastPosition = player.position;
+    }
+
+    void UpdateEnvironment()
+    {
+        bool isIntense = currentSpeed > speedThreshold || enemiesDefeatedRecently > 3;
+
+        directionalLight.color = isIntense ? Color.gray : Color.yellow;
+
+        AudioClip targetClip = isIntense ? intenseClip : calmClip;
+        if (ambientMusic.clip != targetClip)
+        {
+            ambientMusic.clip = targetClip;
+            ambientMusic.Play();
+        }
+    }
+}
